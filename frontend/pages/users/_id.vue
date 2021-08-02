3c51d6fa3c558ea5840744aee06650c1324ac8b8
kazuya takada <takada.chopin.work@gmail.com>
2021-08-02 18:07:40 +0900

    feature:Nuxt側でユーザデータ取得機能の実装#7

--- frontend/pages/users/_id.vue
+++ frontend/pages/users/_id.vue
@@ -18,20 +18,13 @@
       <v-simple-table class="px-5 pb-15">
         <template v-slot:default>
           <tbody>
-            <!-- <tr
-          v-for="item in desserts"
-          :key="item.name"
-        >
-          <td>{{ item.name }}</td>
-          <td>{{ item.calories }}</td>
-        </tr> -->
             <tr>
               <th class="body-1 font-weight-bold">名前</th>
-              <td class="body-1">test</td>
+              <td class="body-1">{{ user.name }}</td>
             </tr>
             <tr>
               <th class="body-1 font-weight-bold">メールアドレス</th>
-              <td class="body-1">test@test.com</td>
+              <td class="body-1">{{ user.email }}</td>
             </tr>
             <tr></tr>
           </tbody>
@@ -42,13 +35,52 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, useContext, onMounted } from '@nuxtjs/composition-api'
+import {
+  defineComponent,
+  useContext,
+  useRoute,
+  reactive,
+  useFetch,
+} from '@nuxtjs/composition-api'
 
 export default defineComponent({
   setup() {
     const { $http } = useContext()
+    const route = useRoute()
 
-    onMounted(() => {})
+    const id = Number(route.value.params.id)
+
+    interface User {
+      allow_password_change: boolean
+      created_at: string
+      email: string
+      id: number
+      name: string
+      provider: string
+      uid: string
+      updated_at: string
+    }
+
+    const user = reactive<User>({
+      allow_password_change: false,
+      created_at: '',
+      email: '',
+      id: 0,
+      name: '',
+      provider: '',
+      uid: '',
+      updated_at: '',
+    })
+
+    useFetch(async () => {
+      const gotUser: User = await $http.$get(`/api/v1/users/${id}`)
+      user.name = gotUser.name
+      user.email = gotUser.email
+    })
+
+    return {
+      user,
+    }
   },
 })
 </script>
