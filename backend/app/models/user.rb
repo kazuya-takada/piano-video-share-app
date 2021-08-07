2528d8a9990f914e361c4d12a932dcd823aa3dd4
kazuya takada <takada.chopin.work@gmail.com>
2021-08-07 15:03:55 +0900

    feature:rails再作成前の状態まで、実装#9

---
+++ backend/app/models/user.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+        :recoverable, :rememberable, :validatable
+  include DeviseTokenAuth::Concerns::User
+  # VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :email, length: { maximum: 72 }
+  # validates :password, format: { with: VALID_PASSWORD_REGEX }
+end
