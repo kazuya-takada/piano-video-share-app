a700f6b4eba3302394ea4ff43f5cc7297fff45eb
takada.chopin.work@gmail.com <takada.chopin.work@gmail.com>
2021-08-15 14:28:58 +0900

    feature:投稿一覧ページの仮レイアウトの作成#21

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,14 +1,11 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center"></v-card>
-      <v-card>
-        <v-card-title class="headline">
-          投稿一覧ページ（予定）
-          {{ user }}
-        </v-card-title>
-      </v-card>
-    </v-col>
+  <v-row align="center">
+    <PostIndivisual />
+    <PostIndivisual />
+    <PostIndivisual />
+    <PostIndivisual />
+    ログインユーザー
+    {{ user }}
   </v-row>
 </template>
 
