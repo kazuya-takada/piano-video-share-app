d0f88b900b8270eb4754c5b4b582c3de4a877ef5
takada.chopin.work@gmail.com <takada.chopin.work@gmail.com>
2021-08-17 16:23:09 +0900

    feature:railsプロジェクトの再作成#23

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,10 +1,3 @@
 Rails.application.routes.draw do
-  namespace :api, format: "json" do
-    namespace :v1 do
-      mount_devise_token_auth_for "User", at: "auth", controllers: {
-        registrations: "api/v1/auth/registrations"
-      }
-			resources :users, only: [:index, :show]
-    end
-  end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
